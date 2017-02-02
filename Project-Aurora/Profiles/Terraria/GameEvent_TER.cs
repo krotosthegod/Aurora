0b105690e7d50f7364bb55a75530473b3e9c440a
KrotosTheGod <krotosthegod@gmail.com>
2017-02-02 03:13:17 -0800

    Simple changes

---
+++ Project-Aurora/Profiles/Terraria/GameEvent_TER.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using Aurora.EffectsEngine;
+using Aurora.Profiles.Terraria.GSI;
+using System.Drawing;
+using Aurora.Profiles.Terraria.GSI.Nodes;
+using System.Linq;
+
+namespace Aurora.Profiles.Terraria
+{
+    public class GameEvent_TER : LightEvent
+    {
+        public GameEvent_TER() : base()
+        {
+        }
+
+        public override void UpdateLights(EffectFrame frame)
+        {
+            Queue<EffectLayer> layers = new Queue<EffectLayer>();
+
+            TERSettings settings = (TERSettings)this.Profile.Settings;
+
+            foreach (var layer in settings.Layers.Reverse().ToArray())
+            {
+                if (layer.Enabled && layer.LogicPass)
+                    layers.Enqueue(layer.Render(_game_state));
+            }
+
+            //Scripts
+            this.Profile.UpdateEffectScripts(layers, _game_state);
+
+            //ColorZones
+            layers.Enqueue(new EffectLayer("TER - Color Zones").DrawColorZones((this.Profile.Settings as TERSettings).lighting_areas.ToArray()));
+
+            frame.AddLayers(layers.ToArray());
+        }
+
+        public override void UpdateLights(EffectFrame frame, IGameState new_game_state)
+        {
+            if (new_game_state is GameState_TER)
+            {
+                _game_state = new_game_state;
+                GameState_TER gs = (new_game_state as GameState_TER);
+
+                try
+                {
+                    UpdateLights(frame);
+                }
+                catch (Exception e)
+                {
+                    Global.logger.LogLine("Exception during OnNewGameState. Error: " + e, Logging_Level.Error);
+                    Global.logger.LogLine(gs.ToString(), Logging_Level.None);
+                }
+            }
+        }
+
+        public override void ResetGameState()
+        {
+            _game_state = new GameState_TER();
+        }
+
+        public override bool IsEnabled()
+        {
+            return this.Profile.Settings.isEnabled;
+        }
+    }
+}
---
+++ Project-Aurora/Profiles/Terraria/TERProfileManager.cs
@@ -0,0 +1,25 @@
+﻿using Aurora.Profiles.Terraroa.GSI;
+using Aurora.Settings;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Aurora.Profiles.Terraria
+{
+    public class TERProfileManager : ProfileManager
+    {
+        public TERProfileManager()
+            : base("Terraria", "ter", "Terraria.exe", typeof(TERSettings), typeof(Control_TER), new GameEvent_TER())
+        {
+            this.AvailableLayers.Add(Aurora.Settings.Layers.LayerType.TERBackground);
+            // this.AvailableLayers.Add(Aurora.Settings.Layers.LayerType.PD2Flashbang);
+            this.AvailableLayers.Add(Aurora.Settings.Layers.LayerType.TERStates);
+
+            IconURI = "Resources/ter_64x64.png";
+        }
+    }
+}
